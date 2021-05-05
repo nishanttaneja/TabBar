50889ec09afe5b71af124c5726baceb7c100ce3f
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-05-05 10:13:00 +0530

    View PopUp Animation

--- TabBar/ViewController.swift
+++ TabBar/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let tabBarC = TabBarController(nibName: nil, bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        initTabBar()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showTabBar()
+    }
 }
 
+extension ViewController {
+    private func initTabBar() {
+        addChild(tabBarC)
+        tabBarC.view.frame.size = CGSize(width: 400, height: 100)   // Should not be required
+        tabBarC.view.frame.origin = .init(x: (self.view.frame.width - self.tabBarC.view.frame.width)/2, y: view.frame.height)
+        tabBarC.view.alpha = 0
+        view.addSubview(tabBarC.view)
+    }
+    
+    private func showTabBar() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.tabBarC.view.alpha = 1
+            self.tabBarC.view.frame.origin.y = self.view.frame.height - self.tabBarC.view.frame.height - self.view.safeAreaInsets.bottom
+        }
+    }
+}
+
+final class TabBarController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
